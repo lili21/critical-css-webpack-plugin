00ed37857f10fad880f3578b32e49ed3d8948e64
li.li <li.li@liulishuo.com>
2018-02-07 18:25:44 +0800

    :tada: hello, critical plugin

---
+++ index.js
@@ -0,0 +1,29 @@
+import path from 'path'
+import critical from 'critical'
+
+export default function CriticalCssWebpackPlugin(options = {}) {
+  this.options = Object.assign({
+    src: 'index.html',
+    dest: 'index.hmlt',
+    inline: true,
+    minify: true,
+    extract: true,
+    width: 375,
+    height: 565,
+    penthouse: {
+      blockJSRequests: false
+    }
+  }, options)
+}
+
+CriticalCssWebpackPlugin.prototype.apply = function(compiler) {
+  compiler.plugin('after-emit', (compilation, callback) => {
+    const base = compilation.outputOptions.path
+
+    const css = Object.keys(compilation.assets)
+      .filter(filename => /\.css$/.test(filename))
+      .map(filename => path.join(base, filename))
+    
+    critical.generate(Object.assign({ base }, this.options), callback)
+  })
+}
